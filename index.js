6e17723cb25ff5f71903d90ad6f0a26e6b9f5103
Alex Morales <kiranightly99@gmail.com>
2020-03-27 18:34:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,6 @@ function takeANumber (katzDeliLine, name) {
 /*Customer comes in to the Deli. He pulls out a tciket, and the ticket gives a number. How would we change the takeANumber function if we dont want to have a line by their names but by numbers?
 
 //0. create a func `takeANumber` (katzDeliLine) => return a number of the customer
-//1. create a variable to hold the counter of customers;
-//2. 
+//1. create a variable to hold the counter of customers in global memory;
+//2. create a variable katzDeliLine = [] in global memory;
+//3. 
